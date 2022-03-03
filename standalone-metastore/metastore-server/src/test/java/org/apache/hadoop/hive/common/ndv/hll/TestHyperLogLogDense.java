374e073d86551441be972106f647625446114260
Ryan Johnson <ryan.johnson@cloudera.com>
2022-03-03 12:13:04 -0800

    Created KLLHistogram Class

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/common/ndv/hll/TestHyperLogLogDense.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/common/ndv/hll/TestHyperLogLogDense.java
@@ -26,6 +26,7 @@ import java.util.Random;
 import java.util.Set;
 
 import org.apache.hadoop.hive.metastore.annotation.MetastoreUnitTest;
+import org.apache.datasketches.kll.KllFloatsSketch;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -80,6 +81,172 @@ public class TestHyperLogLogDense {
     double threshold = size > 40000 ? longRangeTolerance : shortRangeTolerance;
     double delta = threshold * hashset.size() / 100;
     assertEquals((double) hashset.size(), (double) hll.count(), delta);
+  }
+
+  @Test
+  public void testKLLSimple() {
+    // Setup of KLL Sketch and stream the data
+    KllFloatsSketch sketch = new KllFloatsSketch();
+    int size = 1000000;
+    for (int i = 0; i < size; i++) {
+      sketch.update(i);
+    }
+
+    long lenStream = sketch.getN();
+
+    // Create the histogram bins for an equi-height histogram
+    int numBins = 10;
+    long tupsPerBucket = lenStream / numBins;
+    double[] buckets = new double[numBins];
+    double q = 1.0 / numBins;
+    for (int i = 0; i < numBins; i++) {
+      buckets[i] = sketch.getQuantile(q * i);
+    }
+
+    // Confirm that the size streamed is correct
+    double threshold = size > 40000 ? longRangeTolerance : shortRangeTolerance;
+    double delta = threshold * size / 100;
+    assertEquals((double) size, (double) lenStream, delta);
+    System.out.println(Arrays.toString(buckets));
+  }
+
+  @Test
+  public void testKLLSimpleSkewed() {
+    // Setup of KLL Sketch and stream the data
+    // Size variable is the number of elements streamed to the sketch, the first for loop streams 1000000 elements
+    // The second for loop streams 250000 elements
+    KllFloatsSketch sketch = new KllFloatsSketch();
+    int size = 3000000;
+    for (int i = 0; i < 1000000; i++) {
+      sketch.update(i);
+      sketch.update(i);
+    }
+    for (int i = 1000000; i < 2000000; i++) {
+      sketch.update(i);
+    }
+
+    long lenStream = sketch.getN();
+
+    // Create the histogram bins for an equi-height histogram
+    int numBins = 10;
+    long tupsPerBucket = lenStream / numBins;
+    double[] buckets = new double[numBins];
+    double q = 1.0 / numBins;
+    for (int i = 0; i < numBins; i++) {
+      buckets[i] = sketch.getQuantile(q * i);
+    }
+
+    // Confirm that the size streamed is correct
+    double threshold = size > 40000 ? longRangeTolerance : shortRangeTolerance;
+    double delta = threshold * size / 100;
+    assertEquals((double) size, (double) lenStream, delta);
+    System.out.println(Arrays.toString(buckets));
+  }
+
+  @Test
+  public void testKLLSimpleRandom() {
+    // Setup of KLL Sketch and stream the data
+    KllFloatsSketch sketch = new KllFloatsSketch();
+    Random rand = new Random(size);
+    int size = 1000000;
+    for (int i = 0; i < size; i++) {
+      sketch.update(rand.nextInt(500));
+    }
+
+    long lenStream = sketch.getN();
+
+    // Create the histogram bins for an equi-height histogram
+    int numBins = 10;
+    long tupsPerBucket = lenStream / numBins;
+    double[] buckets = new double[numBins];
+    double q = 1.0 / numBins;
+    for (int i = 0; i < numBins; i++) {
+      buckets[i] = sketch.getQuantile(q * i);
+    }
+
+    // Confirm that the size streamed is correct
+    double threshold = size > 40000 ? longRangeTolerance : shortRangeTolerance;
+    double delta = threshold * size / 100;
+    assertEquals((double) size, (double) lenStream, delta);
+    System.out.println(Arrays.toString(buckets));
+  }
+
+  @Test
+  public void testKLLCDF() {
+    KllFloatsSketch sketch = new KllFloatsSketch();
+//    sketch.update(1);
+//    sketch.update(1);
+//    sketch.update(2);
+//    sketch.update(3);
+//    sketch.update(4);
+//    sketch.update(5);
+//    sketch.update(6);
+//    sketch.update(7);
+//    sketch.update(8);
+//    sketch.update(9);
+//    sketch.update(10);
+//    sketch.update(6);
+//    sketch.update(6);
+//    sketch.update(7);
+//    sketch.update(8);
+//    sketch.update(9);
+//    sketch.update(10);
+//    sketch.update(11);
+//    sketch.update(12);
+//    sketch.update(13);
+//    sketch.update(14);
+//    sketch.update(15);
+    sketch.update(1);
+    sketch.update(2);
+    sketch.update(3);
+    sketch.update(4);
+    sketch.update(5);
+    sketch.update(6);
+    sketch.update(7);
+    sketch.update(8);
+    sketch.update(9);
+    sketch.update(10);
+
+
+    float[] temp = new float[2];
+    temp[0] = 8;
+    temp[1] = 9;
+    double[] x = sketch.getCDF(temp);
+    double val = (x[1] - x[0]) / 2;
+    System.out.println(val);
+    System.out.println(Arrays.toString(sketch.getCDF(temp)));
+  }
+
+  @Test
+  public void testKLLHistogram() {
+    KLLHistogram sketch = new KLLHistogram(200);
+    for (int i = 0; i < 1000; i++) {
+      sketch.put(i);
+    }
+    int size = 1000;
+    System.out.println("OUTPUT");
+    System.out.println(sketch.singleSelectivity(990) * size);
+    System.out.println(sketch.rangedSelectivity(200, 550) * size);
+    System.out.println(sketch.greaterThanSelectivity(400) * size);
+    System.out.println(sketch.lessThanSelectivity(200) * size);
+  }
+
+  @Test
+  public void testKLLHistogramSkewed() {
+    KLLHistogram sketch = new KLLHistogram(200);
+    int size = 3000;
+    for (int i = 0; i < 1000; i++) {
+      sketch.put(i);
+      sketch.put(i);
+    }
+    for (int i = 1000; i < 2000; i++) {
+      sketch.put(i);
+    }
+    System.out.println("OUTPUT");
+    System.out.println(sketch.singleSelectivity(10) * size);
+    System.out.println(sketch.rangedSelectivity(200, 550) * size);
+    System.out.println(sketch.greaterThanSelectivity(400) * size);
+    System.out.println(sketch.lessThanSelectivity(400) * size);
   }
 
 }
