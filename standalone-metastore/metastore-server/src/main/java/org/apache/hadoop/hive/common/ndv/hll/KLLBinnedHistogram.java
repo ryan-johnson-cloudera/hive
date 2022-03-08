d0dbd3100a97f34d876f0250de57815bc50696cf
Ryan Johnson <ryan.johnson@cloudera.com>
2022-03-08 09:12:56 -0800

    Created KLL Binned Histogram

---
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/common/ndv/hll/KLLBinnedHistogram.java
@@ -0,0 +1,109 @@
+package org.apache.hadoop.hive.common.ndv.hll;
+
+import org.apache.datasketches.kll.KllFloatsSketch;
+
+
+
+public class KLLBinnedHistogram {
+
+  private KllFloatsSketch sketch;
+  private long lenStream;
+  private int accuracy;
+  private double[] buckets;
+  private long tupsPerBucket;
+
+  public KLLBinnedHistogram(int accuracy) {
+    this.accuracy = accuracy;
+    this.sketch = new KllFloatsSketch(this.accuracy);
+    this.lenStream = 0;
+  }
+
+  public void put(float value) {
+    sketch.update(value);
+    lenStream = sketch.getN();
+  }
+
+  public void computeHistogram(int numBins) {
+    tupsPerBucket = lenStream / numBins;
+    buckets = new double[numBins + 1];
+    double q = 1.0 / numBins;
+    for (int i = 0; i < numBins; i++) {
+      buckets[i] = sketch.getQuantile(q * i);
+    }
+    buckets[numBins] = sketch.getMaxValue();
+  }
+
+  public double rangedSelectivity(float val1, float val2) {
+    double totalTups = 0;
+    int bin = 1;
+    boolean betweenBoundaries = false;
+    double width;
+    double percentageBucket;
+    while (true) {
+      if (!betweenBoundaries && val1 < buckets[bin] && (val2 < buckets[bin] || bin == buckets.length - 1)) {
+        width = buckets[bin] - buckets[bin - 1];
+        percentageBucket = (val2 - val1) / width;
+        totalTups += Math.min(percentageBucket, 1) * tupsPerBucket;
+        break;
+      } else if (!betweenBoundaries && val1 < buckets[bin]) {
+        width = buckets[bin] - buckets[bin - 1];
+        percentageBucket = (buckets[bin] - val1) / width;
+        totalTups += Math.min(percentageBucket, 1 ) * tupsPerBucket;
+        betweenBoundaries = true;
+      } else if (betweenBoundaries && (val2 < buckets[bin] || bin == buckets.length - 1)) {
+        width = buckets[bin] - buckets[bin - 1];
+        percentageBucket = (val2 - buckets[bin - 1]) / width;
+        totalTups += Math.min(percentageBucket, 1) * tupsPerBucket;
+        break;
+      } else if (betweenBoundaries) {
+        totalTups += tupsPerBucket;
+      }
+      bin += 1;
+    }
+    return totalTups / lenStream;
+  }
+
+
+  public double greaterThanSelectivity(float val) {
+    double totalTups = 0;
+    int bin = 1;
+    boolean betweenBoundaries = false;
+    double width;
+    double percentageBucket;
+    while (bin < buckets.length) {
+      if (!betweenBoundaries && val < buckets[bin]) {
+        width = buckets[bin] - buckets[bin - 1];
+        percentageBucket = (buckets[bin] - val) / width;
+        totalTups += Math.min(percentageBucket, 1) * tupsPerBucket;
+        betweenBoundaries = true;
+      } else if (betweenBoundaries) {
+        totalTups += tupsPerBucket;
+      }
+      bin += 1;
+    }
+    return totalTups / lenStream;
+  }
+
+  public double lessThanSelectivity(float val) {
+    double totalTups = 0;
+    int bin = 1;
+    double width;
+    double percentageBucket;
+    while (true) {
+      if (val < buckets[bin] || bin == buckets.length - 1) {
+        width = buckets[bin] - buckets[bin - 1];
+        percentageBucket = (val - buckets[bin - 1]) / width;
+        totalTups += Math.min(percentageBucket, 1) * tupsPerBucket;
+        break;
+      } else {
+        totalTups += tupsPerBucket;
+      }
+      bin += 1;
+    }
+    return totalTups / lenStream;
+  }
+
+  public long lenStream() {
+    return lenStream;
+  }
+}
--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/common/ndv/hll/KLLHistogram.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/common/ndv/hll/KLLHistogram.java
@@ -27,18 +27,22 @@ public class KLLHistogram {
     return (boundaries[1] - boundaries[0]);
   }
 
-  public double singleSelectivity(float val){
+  public double pointSelectivity(float val){
     float offset = (float)(0.01 * lenStream);
     double rangedEstimate = rangedSelectivity(val, val + offset);
     return rangedEstimate / offset;
   }
 
   public double greaterThanSelectivity(float val){
-    return rangedSelectivity(val, sketch.getMaxValue());
+    float[] splitPoints = {val};
+    double[] boundaries = sketch.getCDF(splitPoints);
+    return (1 - boundaries[0]);
   }
 
   public double lessThanSelectivity(float val){
-    return rangedSelectivity(sketch.getMinValue(), val);
+    float[] splitPoints = {val};
+    double[] boundaries = sketch.getCDF(splitPoints);
+    return (boundaries[0]);
   }
 
   public long lenStream(){
